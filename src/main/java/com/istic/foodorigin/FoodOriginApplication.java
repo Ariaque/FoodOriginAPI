8cd1a0cb3fe803eac84154cc9e47d262d1e7f51e
Yaelle <dubois.e1600202@eutd.univ-ubs.fr>
2020-11-03 11:02:05 +0100

    Initial commit

---
+++ src/main/java/com/istic/foodorigin/FoodOriginApplication.java
@@ -0,0 +1,13 @@
+package com.istic.foodorigin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodOriginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodOriginApplication.class, args);
+	}
+
+}
