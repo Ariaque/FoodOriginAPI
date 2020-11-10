43d3f8e56be733d93afa2da3e9ebe113f7e21ffd
Emile <emile.georget@etuidiant.univ-rennes1.fr>
2020-11-10 09:57:18 +0100

    add main auth API logic

--- src/main/java/com/istic/foodorigin/controller/LabelController.java
+++ src/main/java/com/istic/foodorigin/controller/LabelController.java
@@ -3,7 +3,6 @@ package com.istic.foodorigin.controller;
 import com.istic.foodorigin.domain.Label;
 import com.istic.foodorigin.service.LabelService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,19 +13,20 @@ import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 @RestController
+@RequestMapping("/label")
 public class LabelController {
 
     @Autowired
     private LabelService labelService;
 
-    @GetMapping(path = "/labels", produces = "application/json")
+    @GetMapping(path = "/all", produces = "application/json")
     public List<Label> getAllLabel () {
         Iterable<Label> itLab = labelService.getAllLabels();
         List <Label> labels = StreamSupport.stream(itLab.spliterator(), false).collect(Collectors.toList());
         return labels;
     }
 
-    @GetMapping (path = "/label/{id}", produces = "application/json")
+    @GetMapping (path = "/{id}", produces = "application/json")
     public Label getLabelById(@PathVariable Integer id) {
         Label label = labelService.getLabelById(id);
         return label;
