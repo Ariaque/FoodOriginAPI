de69f207ad5f2afe6e78527cb3747f3f0e1e2b89
Yaelle <dubois.e1600202@eutd.univ-ubs.fr>
2021-01-05 17:39:46 +0100

    Add service to post images in the ftp server

--- src/main/java/com/istic/foodorigin/controller/InfosTransformateurController.java
+++ src/main/java/com/istic/foodorigin/controller/InfosTransformateurController.java
@@ -1,9 +1,11 @@
 package com.istic.foodorigin.controller;
 
 import com.istic.foodorigin.models.InfosTransformateur;
+import com.istic.foodorigin.service.ImageService;
 import com.istic.foodorigin.service.InfosTransformateurService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/infoTransformateur")
@@ -11,6 +13,9 @@ public class InfosTransformateurController {
 
     @Autowired
     private InfosTransformateurService infosService;
+
+    @Autowired
+    private ImageService imageService;
 
     @GetMapping (path = "/{id}", produces = "application/json")
     public InfosTransformateur getInfosById (@PathVariable Long id) {
@@ -27,5 +32,10 @@ public class InfosTransformateurController {
     public InfosTransformateur getInfosByTransformateur (@PathVariable Long id) {
         InfosTransformateur infosTransformateur = infosService.getInfosByTransformateur (id);
         return infosTransformateur;
+    }
+
+    @PostMapping (path = "/images/{id}")
+    public boolean saveImage(@RequestParam("myFile") MultipartFile file, @PathVariable Long id) {
+        return imageService.saveImageOnServer(file, id);
     }
 }
